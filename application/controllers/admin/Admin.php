f2954ce59cda9b52ead65517fc4c9e01e9e5abe1
StaffIT22 <137133006+StaffIT22@users.noreply.github.com>
2023-08-16 15:48:14 +0700

    menit 10.50

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -8,7 +8,11 @@ class Admin extends CI_Controller
         $data['title'] = 'My Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
         $this->load->view('admin/index', $data);
+        $this->load->view('templates/footer');
     }
 }
 ?>
---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,47 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" style="font-size:30px" href="index.html">
+    <div class="sidebar-brand-text " >H</div>
+    <i class="fab fa-contao fa-md "></i>
+    <div class="sidebar-brand-text " >G</div>
+    <div class="sidebar-brand-text " >A</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+<div class="sidebar-heading">
+    Administrator
+</div>
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    User
+</div>
+
+<li class="nav-item">
+    <a class="nav-link" href="charts.html">
+    <i class="fas fa-fw fa-users-cog"></i>
+        <span>My Profile</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
