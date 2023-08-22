0922b4d7701726b83cc0b75491ea6867b8fb4abb
StaffIT22 <137133006+StaffIT22@users.noreply.github.com>
2023-08-22 16:00:37 +0700

    25:09

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,9 +11,43 @@
 
 <!-- Divider -->
 <hr class="sidebar-divider">
+<!-- query menu -->
+<?php
+$role_id = $this->session->userdata['role_id'];
+$queryMenu = "  SELECT `user_menu`.`id`,`menu`
+                FROM `user_menu` JOIN `user_access_menu`
+                ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                WHERE `user_access_menu`.`role_id` = $role_id
+                ORDER BY `user_access_menu`.`menu_id` ASC
+             ";
+
+             $menu = $this->db->query($queryMenu)->result_array();
+             
+?>
+
+
+<!-- looping -->
+<?php foreach($menu as $m) ; { ?>
 <div class="sidebar-heading">
-    Administrator
+    <?= $m['menu']; ?>
 </div>
+
+<?php 
+$menuID = $m['id'];
+$querySubMenu = "   SELECT * 
+                    FROM `user_sub_menu` 
+                    WHERE `menu_id` = $menuId 
+                    AND `is_active` = 1
+                ";
+$subMenu = $this->db->query($querySubMenu)->result_array();
+?>
+
+(25:09)
+
+<?php } ?>
+
+
+
 <!-- Nav Item - Dashboard -->
 <li class="nav-item">
     <a class="nav-link" href="index.html">
